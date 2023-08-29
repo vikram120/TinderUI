07ef749cbb75f1ce48b08dc1ec5cf010601b5631
vikram <vikramkunwar1404@gmail.com>
2023-08-29 19:07:36 +0530

    Initial Commit

---
+++ TinderUi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TinderUi
+//
+//  Created by Vikram Kunwar on 29/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
